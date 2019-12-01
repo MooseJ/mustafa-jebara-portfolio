6f27e907ee048415e66df77bf6f4deb6252964ce
Mustafa Jebara <moosejebara@gmail.com>
2019-11-30 22:00:15 -0500

    Add Components for Header, Projects, Work Experience, and About
    Create a basic nav bar which lives in the Header Component.
    It currently allows navigation to the Work Experience page,
    Projects page, and About page.
    
    Created place holder components for Projects, Work Experience,
    and About. These currently display the name of the Component.

---
+++ src/compnents/projects/projects.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './projects.css'
+ 
+export default class Projects extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="projects-title">PROJECTS</h1>
+      </div>
+    )
+  }
+}
