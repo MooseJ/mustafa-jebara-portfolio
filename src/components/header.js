851c254267f3626c914c5418cd3d7816a1e45fc5
Mustafa Jebara <moosejebara@gmail.com>
2020-03-27 15:19:26 -0400

    need this header here

--- src/components/header.js
+++ src/components/header.js
@@ -7,10 +7,10 @@ class Header extends Component {
    
     render() {
         return (
-          <>
+          <header>
             <Logo></Logo>
             <Nav></Nav>
-          </>
+          </header>
         )
       }
     }
