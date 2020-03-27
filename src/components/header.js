1530102f08ad8b1a5fa6e936eb80ca0d33e01a09
Mustafa Jebara <moosejebara@gmail.com>
2020-03-27 15:02:23 -0400

    Resonsive nav, main, and logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
-import './header.css';
+import './Header.css';
+import Logo from './header/Logo';
+import Nav from './header/Nav';
 
 class Header extends Component {
    
     render() {
         return (
-          <header>
-            <div id="logo">
-              <h2>MJ</h2>
-            </div>
-            <nav>
-              <ul>
-                <li>
-                  ABOUT ME
-                </li>
-                <li>
-                  PROJECTS
-                </li>
-                <li>
-                  EXPERIENCE
-                </li>
-                <li>
-                  CONTACT ME
-                </li>
-              </ul>
-            </nav>
-          </header>
+          <>
+            <Logo></Logo>
+            <Nav></Nav>
+          </>
         )
       }
     }
